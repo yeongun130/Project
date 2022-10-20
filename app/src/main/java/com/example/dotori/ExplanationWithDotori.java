36bb7335fa6b1cb16da348bbe59d3b19f72481b2
yeongun130 <yeongunj625@gmail.com>
2022-10-20 19:46:06 +0900

    testproject

---
+++ app/src/main/java/com/example/dotori/ExplanationWithDotori.java
@@ -0,0 +1,14 @@
+package com.example.dotori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExplanationWithDotori extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explanation_with_dotori);
+    }
+}
---
+++ app/src/main/java/com/example/dotori/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.dotori;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageButton button_right;
+    private ImageButton button_left;
+    private Button btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_right = findViewById(R.id.right_button);
+        btn_login = findViewById(R.id.btn_login);
+
+        button_right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ExplanationWithDotori.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
